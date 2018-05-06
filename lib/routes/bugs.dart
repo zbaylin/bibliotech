211769f2d814dd7d45bf48fb44a15e20fe5e8d2f
Ohad Rau <ohadrau@yahoo.com>
2018-05-06 18:44:51 -0400

    Fix formatting

--- lib/routes/bugs.dart
+++ lib/routes/bugs.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:bibliotech/config.dart' as config;
-
 
 Future<Map> submitBugReport(String message) async {
   final response = await http.post("${config.hostname}/report/${config.username}/submit", body: {'message': message});
