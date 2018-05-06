a323467520d58cc524d6e24ba5c0cb48c92b8e96
Ohad Rau <ohadrau@yahoo.com>
2018-05-06 18:43:29 -0400

    Comment config and main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:bibliotech/pages/loading.dart';
 import 'package:bibliotech/pages/login.dart';
 
+// Tells the application what to do on startup. main() is invoked by the
+// operating system when running the app. This in turn delegates the startup
+// procedure to the MyApp widget as its own application.
 void main() => runApp(new MyApp());
 
 // Declares the top widget in the hierarchy, informing it to use Material Design
@@ -20,8 +23,7 @@ class MyApp extends StatelessWidget {
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
+        // or press Run > Flutter Hot Reload in IntelliJ).
         primarySwatch: Colors.blueGrey,
       ),
       // Start at the loading screen while we wait for the main app to load in
